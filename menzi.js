ebc2fc99a75f9a4e0a9736fe6ddf718a9197517d
menzi726 <ivinzondi1996@gmail.com>
2020-09-21 12:30:11 +0200

    added html file

---
+++ menzi.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login Form</title>
+</head>
+<body>
+<div class="login">
+	<input type="text"placeholder="username"
+	id="username"
+	<input type="password"placeholder="password"
+	id="password">
+	<a href="#"class="forgot"forgot password?</a>
+		<input type="submit"value="sign in">
+	
+</div>
+<div class="shadow"></div>
+</body>
+</html>
